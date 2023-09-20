e9381d4f083bca5e0e54663b01682df4f62da2ab
gitkamran <mkamran.ir@gmail.com>
2023-09-21 03:01:00 +0330

    update readme

--- src/layout/Home.js
+++ src/layout/Home.js
@@ -4,7 +4,7 @@ const Home = () => {
 
     return (
         <div className="flex flex-col gap-4 items-center justify-center w-full min-h-screen">
-            <h1>پروژه آماده React</h1>
+            <h1>پروژه برنامه پیش بینی آب و هوا</h1>
         </div>
     )
 }
