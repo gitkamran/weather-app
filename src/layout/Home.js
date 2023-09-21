76cb25530e6a9e0c39c24484065795cf7ce085c0
gitkamran <mkamran.ir@gmail.com>
2023-09-21 23:55:41 +0330

    remove clg

--- src/layout/Home.js
+++ src/layout/Home.js
@@ -15,7 +15,6 @@ const Home = () => {
                 setError(true)
             })
     }, [search])
-    console.log(data)
     return (
         <div className='bg-weather bg-no-repeat bg-cover bg-center w-full h-screen'>
             <div className='w-full h-full flex flex-col items-center gap-12 p-4'>
