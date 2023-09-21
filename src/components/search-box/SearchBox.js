ddbf29dbb74ba52ace0461e223e8b0e18afc16cd
gitkamran <mkamran.ir@gmail.com>
2023-09-21 23:52:52 +0330

    search and home update

--- src/components/search-box/SearchBox.js
+++ src/components/search-box/SearchBox.js
@@ -1,14 +1,31 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { HiOutlineSearch } from 'react-icons/hi'
 
-const SearchBox = () => {
+const SearchBox = ({ setSearch, error }) => {
+    const searchRef = useRef()
+    const search = (e) => {
+        e.preventDefault();
+        if (searchRef.current.value.length > 0) {
+            setSearch(searchRef.current.value)
+            searchRef.current.value = ""
+        }
+    }
     return (
-        <form className='relative w-full h-fit md:w-2/3 lg:w-1/3'>
-            <input className='p-3 rounded-md w-full bg-white bg-opacity-20 outline outline-neutral-700 outline-2 text-neutral-700 text-xl' />
-            <button className='absolute left-2 top-1/2 -translate-y-1/2'>
-                <HiOutlineSearch className='text-3xl text-neutral-700' />
-            </button>
-        </form>
+        <div className='flex flex-col gap-1 w-full h-fit md:w-2/3 lg:w-1/3'>
+            <form onSubmit={search} className='relative'>
+                <input ref={searchRef} placeholder='نام شهر را وارد کنید' className='p-3 rounded-md w-full bg-white bg-opacity-20 outline outline-neutral-700 outline-2 text-neutral-700 text-xl placeholder:text-neutral-600' />
+                <button className='absolute left-2 top-1/2 -translate-y-1/2'>
+                    <HiOutlineSearch className='text-3xl text-neutral-700' />
+                </button>
+            </form>
+            {
+                error &&
+                <div className='w-full bg-neutral-300/70 rounded-md p-2'>
+                    <h2 className='text-neutral-700 text-xl text-center'>شهری یافت نشد</h2>
+                </div>
+            }
+        </div>
+
     )
 }
 
--- src/layout/Home.js
+++ src/layout/Home.js
@@ -4,26 +4,37 @@ import axios from 'axios'
 
 const Home = () => {
     const [data, setData] = useState([-1])
+    const [search, setSearch] = useState("karaj")
+    const [error, setError] = useState(false)
     useEffect(() => {
-        axios.get(`https://api.openweathermap.org/data/2.5/weather?q=Karaj&units=metric&appid=${process.env.REACT_APP_API_KEY}&lang=fa`)
+        axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=${process.env.REACT_APP_API_KEY}&lang=fa`)
             .then(data => {
                 setData(data.data)
+                setError(false)
+            }).catch(err => {
+                setError(true)
             })
-    }, [])
+    }, [search])
     console.log(data)
     return (
         <div className='bg-weather bg-no-repeat bg-cover bg-center w-full h-screen'>
             <div className='w-full h-full flex flex-col items-center gap-12 p-4'>
-                <SearchBox />
+                <SearchBox setSearch={setSearch} error={error} />
+
                 <div className='w-full h-full flex items-center justify-center'>
-                    {data[0] === -1
-                        ?
+                    {data[0] === -1 ?
 
                         <div className='backdrop-blur-md bg-white/10 shadow-md p-8 rounded-md w-full md:w-1/3'>
                             <div className='flex flex-col gap-8'>
-                                <div className='animate-pulse w-1/2 lg:w-1/4 h-9 bg-neutral-700/90 rounded-md'></div>
+                                <div className='flex items-center justify-between gap-4'>
+                                    <div className='animate-pulse w-1/2 lg:w-1/4 h-9 bg-neutral-700/90 rounded-md'></div>
+                                    <div className='flex flex-col items-center gap-1'>
+                                        <div className='animate-pulse w-[100px] h-[100px] bg-neutral-700/90 rounded-md'></div>
+                                        <div className='animate-pulse w-full h-6 bg-neutral-700/90 rounded-md'></div>
+                                    </div>
+                                </div>
                                 <div className='flex items-center justify-center'>
-                                    <div className='animate-pulse w-full lg:w-2/3 h-24 bg-neutral-700/90 rounded-md'></div>
+                                    <div className='animate-pulse w-full lg:w-[40%] xl:w-[40%] h-24 bg-neutral-700/90 rounded-md'></div>
                                 </div>
                                 <div className='flex items-center justify-between w-full gap-8'>
                                     <div className='animate-pulse w-full lg:w-1/3 h-16 bg-neutral-700/90 rounded-md'></div>
@@ -39,11 +50,22 @@ const Home = () => {
                             :
                             <div className='backdrop-blur-md bg-white/10 shadow-md rounded-md p-8 w-full md:w-1/3'>
                                 <div className='flex flex-col gap-8'>
-                                    <h2 className='text-4xl text-neutral-700'>
-                                        {data.name}
-                                    </h2>
+                                    <div className='flex items-center justify-between gap-4'>
+                                        <h2 className='text-4xl text-neutral-700'>
+                                            {data.name}
+                                        </h2>
+                                        <div className='flex flex-col items-center gap-1'>
+                                            <div className=''>
+                                                <img src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`} width={'100%'} height={'100%'} alt={data.weather[0].description} className='w-[100px] h-[100px]' />
+                                            </div>
+                                            <div className='flex flex-col gap-1'>
+                                                <h2 className='text-neutral-800'>{data.weather[0].description}</h2>
+                                            </div>
+                                        </div>
+                                    </div>
 
-                                    <div className='flex items-center justify-center'>
+                                    <div className='flex items-center justify-center gap-1'>
+                                        <span className="text-7xl ltr text-neutral-800">C</span>
                                         <h2 className="text-8xl ltr text-neutral-800">
                                             {Math.trunc(data.main.temp)}°
                                         </h2>
